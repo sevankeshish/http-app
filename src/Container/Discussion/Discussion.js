af118082cac545f43b5a6927c45101119c6296f1
Hexact task for Junior React JS <sevan.keshish0@gmail.com>
2023-06-06 00:11:11 +0400

    getting datas

--- src/Container/Discussion/Discussion.js
+++ src/Container/Discussion/Discussion.js
@@ -1,9 +1,25 @@
+import { useEffect } from "react";
+
+import axios from "axios";
+
 import Comment from "../../Components/Comment/Comment";
 import FullComment from "../../Components/FullComment/FullComment";
 import NewComment from "../../Components/NewComment/NewComment";
+
 import "./discussion.css";
 
 const Discussion = () => {
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <main>
       <section>
